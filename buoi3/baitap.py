2bc4d7ed56a9ee0dcafdb3c8cfea14a2e9e1b680
huy1n <l.huyin1@gmail.com>
2023-06-27 16:15:18 +0700

    buoi3 - update

---
+++ buoi3/baitap.py
@@ -0,0 +1,21 @@
+'''
+1. cho danh sách friends = ['jen', 'jack', 'kenny', 'jelly', 'bob', 'henry', 'anne']
+- lấy ra 4 người bạn đầu tiên trong list
+- lấy ra 4 người bạn cuối cùng trong list
+- đảo ngược list
+- lấy ra những người bạn từ vị trí 1 đến hết
+- copy danh sách ban đầu thành 1 danh sách mới
+- lấy ra những người bạn từ vị trí 2 đến gần cuối
+
+2. cho danh sách student = [['sv001','bob', 25], ['sv002','kenny', 29], ['sv003','anne', 31]]
+- lấy thông tin của sinh viên thứ nhất và in ra dưới định dạng: ID: {id}, name: {name}, age: {age}
+- lấy ra tuổi của sinh viên thứ 2
+- lấy ra thông tin của 2 sinh viên cuối cùng
+- lấy ra id của sinh viên thứ 3
+
+'''
+friends = ['jen', 'jack', 'kenny', 'jelly', 'bob', 'henry', 'anne']
+print(friends)
+print(friends[-4:])
+
+
---
+++ buoi3/buoi3.py
@@ -0,0 +1,56 @@
+numbers = [1, 2, 3, 4]
+# numbers.reverse()
+# print(numbers)
+# index = numbers.index(3)
+# print(index)
+# amount = numbers.count(3)
+# print(amount)
+# amount = numbers.count(1000)
+# print(amount)
+# numbers.clear()
+# print(numbers)
+
+# amount = len(numbers)
+# print(amount)
+
+# numbers.insert(0, 1000)  # đối số đầu tiên là vị trí
+# print(numbers)
+# number.append(5)  # append truyển tham số vào đuôi
+# print(number)
+
+# number.remove(2)
+# print(number)
+
+# last_value = number.pop()
+# print(last_value)
+# print(number)
+
+# number.extend([1, 2, 3 ,45])
+# print(number)
+
+# max_values = max(numbers)
+# min_values = min(numbers)
+# print(max_values)
+# print(min_values)
+'''
+list in list
+copy list
+list slicing
+
+'''
+
+friend = [['john', 23], ['nami', 24], ['cow', 25]]
+print(friend[0][1])
+
+lst1 = [1, 2, 3]
+lst2 = lst1
+
+if lst1 is lst2:
+    print('true')
+
+lst3 = [4, 5, 6]
+lst4 = lst3.copy()
+print(lst3 is lst4)
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(a[:3])  # nó sẽ đếm tới bị trí thứ 3 nhưng không lấy nó
