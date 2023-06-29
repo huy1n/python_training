6003b7d4d56298adf39b433076bd6f4eb6bc04e6
huy1n <l.huyin1@gmail.com>
2023-06-29 13:40:43 +0700

    buoi2 - update

---
+++ buoi2/buoi2.py
@@ -0,0 +1,7 @@
+movies_list = ['naruto','thor','suzume']
+amount = len(movies_list)
+mid = movies_list[amount // 2]
+print(mid)
+
+first = movies_list.pop(0)
+print(first)
---
+++ buoi6/buoi6.py
@@ -0,0 +1,11 @@
+'''
+if - elif - else
+'''
+# Cú pháp:
+# if điều_kiện:
+    # câu_lệnh
+thoi_tiet = 'nang'
+if thoi_tiet == 'nang':
+    print('oke')
+else:
+    print('noo')
